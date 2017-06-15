c615d45ea97b25d697b85f3ab11f8b8ef2781b0c
Erick Kurniawan <erick.kurniawan@gmail.com>
2017-06-15 11:36:30 +0700

    menambahkan form edit

---
+++ SampleServices/SampleServices/EditData.xaml.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace SampleServices
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class EditData : ContentPage
+    {
+        public EditData()
+        {
+            InitializeComponent();
+        }
+    }
+}
